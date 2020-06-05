f0f952de04902801bef61834bcb9e180a2805361
student <ff@>
2020-06-05 16:10:27 +0800

    2020上機考版本1(楊適育)

---
+++ app/src/main/java/tw/edu/pu/s1071928/trafficlight/GameActivity.java
@@ -0,0 +1,78 @@
+package tw.edu.pu.s1071928.trafficlight;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class GameActivity extends AppCompatActivity {
+
+    GameSurfaceView GameSV;
+    Handler handler;
+    Handler lightTimer;
+    AlertDialog.Builder alertDialog;
+    View decorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //設定全螢幕顯示
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //設定螢幕為橫式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        setContentView(R.layout.activity_game);
+
+        GameSV = (GameSurfaceView) findViewById(R.id.GameSV);
+        //設定初始測試之燈號秒數
+        GameSV.SetLightSec(it.getIntExtra("green", 0),
+                it.getIntExtra("yellow", 0),
+                it.getIntExtra("red", 0));
+
+        handler = new Handler();
+        lightTimer = new Handler();
+        alertDialog = new AlertDialog.Builder(this)
+                .setTitle("遊戲結束")
+                .setIcon(R.drawable.boy1)
+                .setPositiveButton("再玩一次", this)
+                .setNegativeButton("結束系統", this)
+                .setNeutralButton("設定燈號秒數", this);
+    }
+
+    //利用手指觸控，控制小男孩走路
+    public boolean onTouchEvent (MotionEvent event){
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            GameSV.BoyMoving = true;
+            handler.post(runnable);
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP){
+            GameSV.BoyMoving =  false;
+            handler.removeCallbacks(runnable);  //銷毀執行緒
+        }
+        return true;
+    }
+
+    //處理小男孩走路
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Canvas canvas = GameSV.getHolder().lockCanvas();
+            GameSV.drawSomething(canvas);
+            GameSV.getHolder().unlockCanvasAndPost(canvas);
+            handler.postDelayed(runnable, 50);
+        }
+    };
+}
---
+++ app/src/main/java/tw/edu/pu/s1071928/trafficlight/MainActivity.java
@@ -0,0 +1,61 @@
+package tw.edu.pu.s1071928.trafficlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText greenText;
+    EditText yellowText;
+    EditText redText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //設定全螢幕顯示
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //設定螢幕為橫式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void StartGame(View v){
+        String g = greenText.getText().toString();
+        String y = yellowText.getText().toString();
+        String r = redText.getText().toString();
+
+        if (g.equals("") || y.equals("") || r.equals("")) {
+            Toast.makeText(this, "燈號的秒數不能為空白", Toast.LENGTH_LONG).show();
+        } else if (g.equals("0") || y.equals("0") || r.equals("0")) {
+            Toast.makeText(this, "燈號的秒數不能為0", Toast.LENGTH_LONG).show();
+        } else {
+            Intent it = new Intent();
+            it.setClass(this, GameActivity.class);
+            it.putExtra("green", Integer.parseInt(g));
+            it.putExtra("yellow", Integer.parseInt(y));
+            it.putExtra("red", Integer.parseInt(r));
+            startActivity(it);
+            finish();
+        }
+    }
+
+    public void EndApp(View v){
+        finish();
+    }
+}
